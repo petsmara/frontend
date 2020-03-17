bec471166fa65c36b9261c6c35aac1940c5ae909
Duckhee Lee <shldhee@yahoo.co.kr>
2020-03-18 01:09:46 +0900

    :art: Fix axios logic
    - Write global option in main.js

--- src/main.js
+++ src/main.js
@@ -5,14 +5,11 @@ import store from './store/store'
 import axios from 'axios'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
-// import '@/assets/css/element-variables.scss'
-// import '@/assets/css/style.scss'
-// import { Button, Input, Form, FormItem } from 'element-ui'
-Vue.use(ElementUI)
-// Vue.component(Button)
-// Vue.component(Select)
 
+Vue.use(ElementUI)
 Vue.config.productionTip = false
+
+axios.defaults.baseURL = 'http://52.78.166.10:8000'
 
 new Vue({
   router,
@@ -24,5 +21,14 @@ new Vue({
       const userData = JSON.parse(userString)
       this.$store.commit('user/SET_USER_DATA', userData)
     }
+    axios.interceptors.response.use(
+      response => response,
+      error => {
+        if (error.response.status === 401) {
+          // this.$store.dispatch('logout')
+        }
+        return Promise.reject(error)
+      }
+    )
   }
 }).$mount('#app')
--- src/services/ProductService.js
+++ src/services/ProductService.js
@@ -1,27 +1,9 @@
 import axios from 'axios'
-
-const apiClient = axios.create({
-  baseURL: `http://52.78.166.10:8000`,
-  withCredentials: false, // This is the default
-  headers: {
-    Accept: 'application/json',
-    'Content-Type': 'application/json'
-  },
-  timeout: 15000
-})
-
 export default {
-  // getUsers(perPage, page) {
-  //   return apiClient.get('/Users?_limit=' + perPage + '&_page=' + page)
-  // },
-  // getUser(id) {
-  //   return apiClient.get('/Users/' + id)
-  // },
   uploadImages(images) {
-    return apiClient.post('/image', images)
+    return axios.post('/image', images)
   },
   registerProduct(product) {
-    return apiClient.post('/product', product)
+    return axios.post('/product', product)
   }
-  // logIn(user) {}
 }
