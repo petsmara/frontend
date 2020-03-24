552fee21642119ca8fa769fcf4768a5a155239b3
Duckhee Lee <shldhee@yahoo.co.kr>
2020-03-24 12:56:26 +0900

    Add feature BackToTop Fix style

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -19,14 +19,19 @@
     </section>
 
     <section class="content">
-      <Box class="content__box">
-        <template #title>
-          NEW ITEM
-        </template>
-        <template #content>
-          새로운 사료를 만나보세요
-        </template>
-      </Box>
+      <div class="content__header">
+        <Box class="content__box">
+          <template #title>
+            NEW ITEM
+          </template>
+          <template #content>
+            새로운 사료를 만나보세요
+          </template>
+        </Box>
+        <button class="content__write">
+          글쓰기
+        </button>
+      </div>
       <div class="products">
         <router-link
           class="products__link"
@@ -43,10 +48,10 @@
           />
         </router-link>
       </div>
-      <button class="products__write">
-        글쓰기
-      </button>
     </section>
+    <template>
+      <el-backtop></el-backtop>
+    </template>
   </div>
 </template>
 
@@ -147,9 +152,47 @@ export default {
   }
 }
 .content {
+  position: relative;
+  &__header {
+    position: relative;
+  }
   &__box {
     margin: 72px auto;
     text-align: center;
+  }
+  &__write {
+    @include respond-to('mobile-portrait-only') {
+      z-index: 5;
+      position: fixed;
+      bottom: 40px;
+      left: 50%;
+      transform: translateX(-50%);
+    }
+    @include respond-to('tablet-portrait-only') {
+    }
+    cursor: pointer;
+    font-size: 16px;
+    color: #000000;
+    padding: 10px 40px 10px 66px;
+    position: absolute;
+    right: 0;
+    top: 50%;
+    transform: translateY(-50%);
+    background: #e9e9e9;
+    border-radius: 8px;
+    border: none;
+    margin: 0 10px 0 auto;
+    &::after {
+      content: '';
+      position: absolute;
+      left: 30px;
+      top: 50%;
+      transform: translateY(-50%);
+      width: 26px;
+      height: 20px;
+      background: url('~@/assets/images/icons/close.png') center / 100%
+        no-repeat;
+    }
   }
 }
 .products {
@@ -169,28 +212,6 @@ export default {
     @include respond-to('tablet-portrait-only') {
       flex-basis: 50%;
       // background-color: red;
-    }
-  }
-  &__write {
-    cursor: pointer;
-    font-size: 16px;
-    color: #000000;
-    padding: 10px 40px 10px 66px;
-    position: relative;
-    background: #fd9f9f;
-    border-radius: 8px;
-    border: none;
-    margin: 0 10px 0 auto;
-    &::after {
-      content: '';
-      position: absolute;
-      left: 30px;
-      top: 50%;
-      transform: translateY(-50%);
-      width: 26px;
-      height: 20px;
-      background: url('~@/assets/images/icons/close.png') center / 100%
-        no-repeat;
     }
   }
 }
--- src/views/product/ProductList.vue
+++ src/views/product/ProductList.vue
@@ -8,14 +8,19 @@
       />
     </section>
     <section class="content">
-      <Box class="content__box">
-        <template #title>
-          NEW ITEM
-        </template>
-        <template #content>
-          새로운 사료를 만나보세요
-        </template>
-      </Box>
+      <div class="content__header">
+        <Box class="content__box">
+          <template #title>
+            NEW ITEM
+          </template>
+          <template #content>
+            새로운 사료를 만나보세요
+          </template>
+        </Box>
+        <button class="content__write">
+          글쓰기
+        </button>
+      </div>
       <div class="products">
         <router-link
           class="products__link"
@@ -32,27 +37,9 @@
           />
         </router-link>
       </div>
-      <button class="products__write">
-        글쓰기
-      </button>
     </section>
     <template>
-      Scroll down to see the bottom-right button.
-      <el-backtop target=".product-list" :bottom="100">
-        <div
-          style="{
-        height: 100%;
-        width: 100%;
-        background-color: #f2f5f6;
-        box-shadow: 0 0 6px rgba(0,0,0, .12);
-        text-align: center;
-        line-height: 40px;
-        color: #1989fa;
-      }"
-        >
-          UP
-        </div>
-      </el-backtop>
+      <el-backtop></el-backtop>
     </template>
   </div>
 </template>
@@ -113,9 +100,46 @@ export default {
 }
 
 .content {
+  &__header {
+    position: relative;
+  }
   &__box {
     margin: 72px auto;
     text-align: center;
+  }
+  &__write {
+    @include respond-to('mobile-portrait-only') {
+      z-index: 5;
+      position: fixed;
+      bottom: 40px;
+      left: 50%;
+      transform: translateX(-50%);
+    }
+    @include respond-to('tablet-portrait-only') {
+    }
+    cursor: pointer;
+    font-size: 16px;
+    color: #000000;
+    padding: 10px 40px 10px 66px;
+    position: absolute;
+    right: 0;
+    top: 50%;
+    transform: translateY(-50%);
+    background: #e9e9e9;
+    border-radius: 8px;
+    border: none;
+    margin: 0 10px 0 auto;
+    &::after {
+      content: '';
+      position: absolute;
+      left: 30px;
+      top: 50%;
+      transform: translateY(-50%);
+      width: 26px;
+      height: 20px;
+      background: url('~@/assets/images/icons/close.png') center / 100%
+        no-repeat;
+    }
   }
 }
 .products {
@@ -135,28 +159,6 @@ export default {
     @include respond-to('tablet-portrait-only') {
       flex-basis: 50%;
       // background-color: red;
-    }
-  }
-  &__write {
-    cursor: pointer;
-    font-size: 16px;
-    color: #000000;
-    padding: 10px 40px 10px 66px;
-    position: relative;
-    background: #fd9f9f;
-    border-radius: 8px;
-    border: none;
-    margin: 0 10px 0 auto;
-    &::after {
-      content: '';
-      position: absolute;
-      left: 30px;
-      top: 50%;
-      transform: translateY(-50%);
-      width: 26px;
-      height: 20px;
-      background: url('~@/assets/images/icons/close.png') center / 100%
-        no-repeat;
     }
   }
 }
